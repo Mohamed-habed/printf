58134f2d33f8b0cd03e5dbfa265d216c8ede4041
Yanbel0706 <133171485+Yanbel0706@users.noreply.github.com>
2023-07-27 13:49:49 +0100

    Update 2_printf.c

--- _printf.c
+++ _printf.c
@@ -54,7 +54,7 @@ int _printf(const char *format, ...)
 
 /**
  * @buffer: A doc line
- *print_buffer - Prints the contents of the buffer if it exist
+ * print_buffer - Prints the contents of the buffer if it exist
  * @buff_ind: Index at which to add next char, represents the length.
  */
 void print_buffer(char buffer[], int *buff_ind)
